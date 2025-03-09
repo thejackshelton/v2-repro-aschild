90c1e46b6acf851ecf0c3871cf94ae9ad5de020a
thejackshelton <me@jackshelton.com>
2025-03-09 17:01:36 -0500

    ariachecked

--- src/components/checkbox/checkbox-trigger.tsx
+++ src/components/checkbox/checkbox-trigger.tsx
@@ -1,12 +1,4 @@
-import {
-	$,
-	type PropsOf,
-	Slot,
-	component$,
-	sync$,
-	useComputed$,
-	useContext,
-} from "@qwik.dev/core";
+import { $, type PropsOf, Slot, component$, useContext } from "@qwik.dev/core";
 import { withAsChild } from "../as-child/as-child";
 import { Render } from "../render/render";
 import { checkboxContextId } from "./checkbox-context";
@@ -17,18 +9,6 @@ export const CheckboxTriggerBase = component$(
 	(props: PublicCheckboxControlProps) => {
 		const context = useContext(checkboxContextId);
 		const triggerId = `${context.localId}-trigger`;
-		const descriptionId = `${context.localId}-description`;
-		const errorId = `${context.localId}-error`;
-		const describedByLabels = useComputed$(() => {
-			const labels = [];
-			if (context.description) {
-				labels.push(descriptionId);
-			}
-			if (context.isErrorSig.value) {
-				labels.push(errorId);
-			}
-			return labels.join(" ") || undefined;
-		});
 		const handleClick$ = $(() => {
 			if (context.isCheckedSig.value === "mixed") {
 				context.isCheckedSig.value = true;
@@ -37,11 +17,11 @@ export const CheckboxTriggerBase = component$(
 			}
 		});
 
-		const handleKeyDownSync$ = sync$((e: KeyboardEvent) => {
-			if (e.key === "Enter") {
-				e.preventDefault();
-			}
-		});
+		// const handleKeyDownSync$ = sync$((e: KeyboardEvent) => {
+		// 	if (e.key === "Enter") {
+		// 		e.preventDefault();
+		// 	}
+		// });
 
 		return (
 			<Render
@@ -50,19 +30,12 @@ export const CheckboxTriggerBase = component$(
 				type="button"
 				role="checkbox"
 				fallback="button"
-				aria-checked={`${context.isCheckedSig.value}`}
-				aria-describedby={
-					describedByLabels ? describedByLabels.value : undefined
-				}
+				aria-checked={context.isCheckedSig.value}
 				aria-invalid={context.isErrorSig.value}
 				disabled={context.isDisabledSig.value}
 				// Indicates whether the checkbox trigger is disabled
 				data-disabled={context.isDisabledSig.value ? "" : undefined}
-				onKeyDown$={[handleKeyDownSync$, props.onKeyDown$]}
 				onClick$={[handleClick$, props.onClick$]}
-				// Indicates whether the checkbox trigger is checked
-				// Indicates whether the checkbox trigger is in an indeterminate state
-				// Identifier for the checkbox trigger element
 				data-qds-checkbox-trigger
 				{...props}
 			>
